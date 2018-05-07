ab2731149d519d510be041df0349078578ac66ec
Angus Rush <angus.h.rush@hotmail.com>
2018-05-07 11:27:11 +0100

    Many changes

--- emacs.el
+++ emacs.el
@@ -12,7 +12,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (linum-relative evil-exchange nord-theme evil-surround evil auctex ##))))
+    (yasnippet adaptive-wrap linum-relative evil-exchange nord-theme evil-surround evil auctex ##))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -21,21 +21,45 @@
  ;; If there is more than one, they won't work right.
  )
 
+;; Evil mode!
 (require 'evil)
 (evil-mode 1)
+(setq evil-normal-state-modes (append evil-motion-state-modes evil-normal-state-modes))
+(setq evil-motion-state-modes nil)
+
+;; Ultisnips for emacs
+(require 'evil-exchange)
+
+;; Spaces instead of tabs
+(setq-default indent-tabs-mode nil)
+
+;; Evil surround
+(global-evil-surround-mode 1)
 
 (require 'evil-exchange)
 (evil-exchange-install)
 
+;; Nice color scheme
 (require 'nord-theme)
 
-(setq evil-normal-state-modes (append evil-motion-state-modes evil-normal-state-modes))
-(setq evil-motion-state-modes nil)
+;; Relative Line numbers
+(linum-relative-global-mode)
 
+;; Smooth scrolling
+(setq scroll-margin 5
+      scroll-conservatively 9999
+      scroll-step 1)
+
+;; Indent wrapped lines correctly
+(require 'adaptive-wrap)
+(adaptive-wrap-prefix-mode)
+
+;; Disable gui nonsense
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (toggle-scroll-bar -1)
 
-(set-default-font "Source Code Pro for Powerline 10")
+;; A nice font
+(set-default-font "Source Code Pro for Powerline 9")
 
-; vim: filetype=lisp
+;; vim: filetype=lisp
